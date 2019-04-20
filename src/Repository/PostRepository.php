07ef15793b369b37a4138312be7ada2be79bf115
Pierre KELBERT <pierre.kelbert@kleeft.fr>
2019-04-20 17:17:19 -0400

    fix sql error

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -9,7 +9,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 /**
  * @method Post|null find($id, $lockMode = null, $lockVersion = null)
  * @method Post|null findOneBy(array $criteria, array $orderBy = null)
- * @method Post[]    findAll()
+
  * @method Post[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class PostRepository extends ServiceEntityRepository
